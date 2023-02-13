2769fe503eceeb8cb4d20da2881d9157bb852fd3
ShivsTKD <77997932+ShivsTKD@users.noreply.github.com>
2023-02-13 09:27:36 -0400

    update lazy=dynamic in Topic model

--- App/models/topic.py
+++ App/models/topic.py
@@ -6,9 +6,9 @@ class Topic(db.Model):
     name = db.Column(db.String(60), unique=True, nullable=False)
     parent_topic_id = db.Column(db.Integer, ForeignKey("topic.id"), nullable=True)
     parent_topic = db.relationship("Topic", backref="subtopics", remote_side=id)
-    researcher_tags = db.relationship("ResearcherTag", backref="topic", lazy=True, cascade="all, delete-orphan")
-    pub_tags = db.relationship("PublicationTag", backref="topic", lazy=True, cascade="all, delete-orphan")
-    sub_records = db.relationship("TopicSubRecord", backref="subject", lazy=True, cascade="all, delete-orphan")
+    researcher_tags = db.relationship("ResearcherTag", backref="topic", lazy="dynamic", cascade="all, delete-orphan")
+    pub_tags = db.relationship("PublicationTag", backref="topic", lazy="dynamic", cascade="all, delete-orphan")
+    sub_records = db.relationship("TopicSubRecord", backref="subject", lazy="dynamic", cascade="all, delete-orphan")
 
     def __init__(self, name):
         self.name = name
