43791493d8b42b5df1dc0a337d7d0ab107dc8552
ShivsTKD <shivanmaharaj.sm@gmail.com>
2023-02-17 20:13:54 +0000

    Got JS To Work With Flask Sending Requests

--- App/static/main.js
+++ App/static/main.js
@@ -1,1 +1,29 @@
-UIkit.img(element, options);
+let researcher_form = document.getElementById("researcher_signup");
+
+// researcher_form.addEventListener("onsubmit", function(event){
+//     alert('This not working?');
+//     event.preventDefault();
+//     researcher_form.innerHTML = `<h1>TEST</h1>`;
+//     return false;
+// });
+
+
+async function trial(){
+    let x = 0;
+    let researcher_form = document.getElementById("researcher_signup");
+    let selected = document.getElementsByClassName('selected');
+    let selected_items = [];
+    for (let i of selected){
+        selected_items[x] = i.innerHTML;
+        x += 1;
+    };
+    alert(selected_items);
+    selected = await getResearchInterests(selected_items);
+    researcher_form.preventDefault;
+};
+
+
+
+async function getResearchInterests(selected){
+    return await fetch(`/interests/${JSON.stringify({'selected': selected})}`);
+};
--- App/views/index.py
+++ App/views/index.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.models.forms import *
+import json
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -15,3 +16,10 @@ def login_page():
 def signup_page():
     form = ResearcherSignUpForm()
     return render_template('signup.html', form=form)
+
+@index_views.route('/interests/<selected>', methods=['GET'])
+def parse_interests(selected):
+    selected = json.loads(selected)
+    print(selected)
+    print(selected['selected'][0]) #AMAZING
+    return jsonify(selected)
