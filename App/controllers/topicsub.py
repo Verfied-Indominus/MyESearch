0a1b46c86127fdab60a3810c1014e491c75992d5
TreyMurray01 <109750453+TreyMurray01@users.noreply.github.com>
2023-02-19 14:48:28 +0000

    topic subscribers done

--- App/controllers/topicsub.py
+++ App/controllers/topicsub.py
@@ -1,5 +1,6 @@
 from App.models import TopicSubRecord
 from . import user, topic
+from App.database import db
 
 def create_sub(tid,uid):
     try:
@@ -18,9 +19,25 @@ def create_sub(tid,uid):
 def get_subs(tid):
     return TopicSubRecord.query.filter_by(id=tid).all()
 
-
-def delete_sub():
-    pass
-
-def delete_sub():
-    pass
+def delete_sub_id(sid):
+    try:
+        sub = TopicSubRecord.query.filter_by(id=sid).first()
+        if sub is None:
+            return False
+        db.session.delete(sub)
+        db.session.commit()
+    except:
+        return False
+        
+def delete_sub(tid,uid):
+    try:
+        use = user.get_user(uid)
+        top = topic.get_topic_id(tid)
+        if top is None or use is None:
+            return False
+        sub  = TopicSubRecord.query.filter_by(user_id = uid,topic_id=tid).first()
+        db.session.delete(sub)
+        db.session.commit()
+        return True
+    except:
+        return False
