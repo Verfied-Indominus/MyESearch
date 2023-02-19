d471c0ab66de095e6f78063aad504d9ad1a9fa66
TreyMurray01 <109750453+TreyMurray01@users.noreply.github.com>
2023-02-19 21:43:40 +0000

    researcher notification

--- App/controllers/researchersubs.py
+++ App/controllers/researchersubs.py
@@ -1,6 +1,6 @@
 from App.models import ResearcherSubRecord
 from App.database import db
-from . import researcher, user
+from . import researcher, user,publication,notification
 
 def create_sub(re_id, uid):
     try:
@@ -8,7 +8,7 @@ def create_sub(re_id, uid):
         sub = user.get_user(uid)
         if re == None or uid == None:
             return False
-        new_re_sub = ResearcherSubRecord(user_id, researcher_id)
+        new_re_sub = ResearcherSubRecord(uid, re_id)
         db.session.add(new_re_sub)
         db.session.commit()
         return True
@@ -16,7 +16,7 @@ def create_sub(re_id, uid):
         return False
         
 
-def remove_sub(sub_id):
+def remove_sub_id(sub_id):
     try:
         query = ResearcherSubRecord.query.filter_by(id=sub_id).first()
         db.session.delete(query)
@@ -37,6 +37,18 @@ def remove_sub(re_id,uid):
         query = ResearcherSubRecord.query.filter_by(researcher_id=re_id,user_id=uid).first()
         db.session.delete(query)
         db.session.commit()
+        return True
+    except:
+        return False
+
+def researcher_notification(re_id,pid):
+    try:
+        re = researcher.get_researcher(re_id)
+        subs = get_subs(re_id=re_id)
+        pub = publication.get_pub_byid(id=pid)
+        title = f"New publication from: {re.first_name} {re.last_name}"
+        message = f"New publication added: {pub.title}"
+        notification.notify_subscribers(subs,title,message)
         return True
     except:
         return False
