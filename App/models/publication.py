c2a3b05c39ff48a646aabe6dd4c58d52ec6f4cc2
shiv-backup <101745134+shiv-backup@users.noreply.github.com>
2023-02-25 22:44:41 -0400

    Added DOI attribute to Publication Model

--- App/models/publication.py
+++ App/models/publication.py
@@ -12,6 +12,7 @@ class Publication(db.Model):
     downloads = db.Column(db.Integer, nullable=False)
     searches = db.Column(db.Integer, nullable=False)
     cover = db.Column(db.String(120))
+    doi = db.Column(db.String(120))
     tags = db.relationship("PublicationTag", backref="publication", lazy="dynamic", cascade="all, delete-orphan")
     pub_records = db.relationship("PubRecord", backref="publication", lazy="dynamic", cascade="all, delete-orphan")
     lib_records = db.relationship("LibraryRecord", backref="publication", lazy="dynamic", cascade="all, delete-orphan")
