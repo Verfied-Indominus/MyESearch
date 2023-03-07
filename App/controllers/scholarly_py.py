4547ae1d6209d94eed9a05a13a834b669c706522
ShivsTKD <77997932+ShivsTKD@users.noreply.github.com>
2023-03-07 04:51:29 +0000

    Made Changes to Scholarly Calls Included search_pubs if author obj not found

--- App/controllers/scholarly_py.py
+++ App/controllers/scholarly_py.py
@@ -1,17 +1,59 @@
 from scholarly import scholarly, ProxyGenerator
 
 pg = ProxyGenerator()
-pg.FreeProxies()
-scholarly.use_proxy(pg)
 
-search_query = scholarly.search_author('Amit Neil Ramkissoon')
+def set_new_proxy():
+    while True:
+        success = pg.FreeProxies()
+        if success:
+            scholarly.use_proxy(pg)
+            break
+
+first_author_result = None
+
+# search_query = scholarly.search_author('Permanand Mohan')
 # Retrieve the first result from the iterator
-first_author_result = next(search_query)
+# try:
+#     first_author_result = next(search_query)
+# except Exception as e:
+#     print(e.__repr__)
 # scholarly.pprint(first_author_result)
 #Returns personal info of author
 #Scholar ID is important for verifications
 # print(first_author_result)
+# If author query yields no result
+i = 0
 
+if not first_author_result:
+    while True:
+        try:
+            pub_query = scholarly.search_pubs('Permanand Mohan')
+            break
+        except Exception as e:
+            i += 1
+            print("Author", i)
+            set_new_proxy()
+
+filled_pubs = []
+while True:
+    try:
+        for pub in pub_query:
+            while True:
+                try:
+                    fill = scholarly.fill(pub)
+                    filled_pubs.append(fill)
+                    print(fill)
+                    print('\n')
+                    break
+                except Exception as e:
+                    i += 1
+                    print("Pub", i)
+                    set_new_proxy()
+        break
+    except Exception as e:
+        i += 1
+        print("Pub", i)
+        set_new_proxy()
 
 # Pull specifics for the author ["basics","indices","counts","coauthors","publications"]
 # 'basics' = name, affiliation, and interests;
@@ -21,7 +63,7 @@ first_author_result = next(search_query)
 # 'publications' = publications
 # '[]' = all of the above (this is the default)
 # e.g scholarly.fill(author,["basics","indices","counts","coauthors","publications"]) returns ALL info on the author
-author = scholarly.fill(first_author_result )
+# author = scholarly.fill(first_author_result)
 # scholarly.pprint(author)
 # print(author)
 
@@ -32,8 +74,8 @@ author = scholarly.fill(first_author_result )
 # print(first_publication_filled)
 
 # # Print the titles of the author's publications
-publication_titles = [pub['bib']['title'] for pub in author['publications']]
-print(publication_titles)
+# publication_titles = [pub['bib']['title'] for pub in author['publications']]
+# print(publication_titles)
 
 # # Which papers cited that publication?
 # citations = [citation['bib']['title'] for citation in scholarly.citedby(first_publication_filled)]
