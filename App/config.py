8546010b234d1a08d6cdb7fec109d98fe53d06f1
ShivsTKD <shivanmaharaj.sm@gmail.com>
2023-12-29 17:01:35 +0000

    Reset DB

--- App/config.py
+++ App/config.py
@@ -1,6 +1,6 @@
 # SQLALCHEMY_DATABASE_URI = "sqlite:///temp.db"
 # SQLALCHEMY_DATABASE_URI = "postgresql://my_e_search_db_user:llH7VQdxuiQyT7fb7xZbqdAnYErYKmVh@dpg-cfgml9hgp3jjseebr8c0-a.oregon-postgres.render.com/my_e_search_db"
-SQLALCHEMY_DATABASE_URI = "postgresql://myesearch_user:8XHl2xTdUe7RRCDy4mX10b9GgaJ9gDJv@dpg-cgn4e9t269v6fnsp1nbg-a.ohio-postgres.render.com/myesearch"
+SQLALCHEMY_DATABASE_URI = "postgresql://myesearch_jkkz_user:RsV8xACG6fLABHRSTJezi8d1SPNq20HZ@dpg-cm7flkocmk4c738tfogg-a.oregon-postgres.render.com/myesearch_jkkz"
 # PASSWORD = "llH7VQdxuiQyT7fb7xZbqdAnYErYKmVh"
 PASSWORD = "8XHl2xTdUe7RRCDy4mX10b9GgaJ9gDJv"
 SECRET_KEY = "secret key"
